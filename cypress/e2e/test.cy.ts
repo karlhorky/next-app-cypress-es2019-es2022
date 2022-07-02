0e4332db046035efaaa6f07a543d1b9dec05faa2
Karl Horky <karl.horky@gmail.com>
2022-07-02 12:31:50 +0200

    Add assertion, still passing

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,6 +1,7 @@
 describe('Optional chaining', () => {
   it('passes', () => {
     let something = [1, 2, 3, 4, 5] as number[] | undefined;
+    expect(something?.at(0)).to.equal(1);
     expect(something?.length).to.equal(5);
   });
 });
