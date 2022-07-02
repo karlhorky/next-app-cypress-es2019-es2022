a0872eb122c35c69b8a11f501fde641fe4a6f65d
Karl Horky <karl.horky@gmail.com>
2022-07-02 12:34:26 +0200

    Switch to unsupported syntax, tsc fails

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,6 +1,8 @@
 describe('Optional chaining', () => {
   it('passes', () => {
-    let something = [1, 2, 3, 4, 5] as number[] | undefined;
+    let something = Object.freeze([1, 2, 3, 4, 5]) as
+      | readonly number[]
+      | undefined;
     expect(something?.at(0)).to.equal(1);
     expect(something?.length).to.equal(5);
   });
